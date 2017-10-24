5d0ade43ebbe2487be40a651409109afeeb3c9ac
Malgorzata Haniszewska <gosia.haniszewska@gmail.com>
2017-10-23 20:49:41 -0700

    change html that user uses one label and two buttons, one to add to list and one to submit the list

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,17 @@
 $(document).ready(function() {
 
+  var items = [];
+
+  $("button#addItem").click(function() {
+    var item = $("input#item").val().toUpperCase();
+    items.push(item);
+  });
+
   $("form#inputItem").submit(function(event) {
 
-    var item1 = $("input#item1").val().toUpperCase();
-    var item2 = $("input#item2").val().toUpperCase();
-    var item3 = $("input#item3").val().toUpperCase();
-    var item4 = $("input#item4").val().toUpperCase();
-    var item5 = $("input#item5").val().toUpperCase();
-
-    var items=[item1, item2, item3, item4, item5];
-
     items.sort().forEach(function(item) {
-      $("li#shoppingItem").append("<li>" + item + "</li>");
+      $("#shoppingItem").append("<li>" + item + "</li>");
     });
-
-
-
 
     $("#inputItem").hide();
     $(".shoppingList").show();
